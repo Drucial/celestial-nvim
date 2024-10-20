3b63106b931af35382833e4759645f5c9896dd11
Drucial <jdwhite32@gmail.com>
2024-10-20 12:02:20 -0400

    change colors.lua to match celestial theme

--- lua/cyberdream/colors.lua
+++ lua/cyberdream/colors.lua
@@ -3,74 +3,74 @@ local M = {}
 
 ---@class CyberdreamColorLight
 ---@field bg "#ffffff"|string
----@field bgAlt "#f7f8f9"|string
----@field bgHighlight "#e9eef2"|string
+---@field bgAlt "#eaeaea"|string
+---@field bgHighlight "#acacac"|string
 ---@field bg_solid? string
 ---@field fg "#16181a"|string
 ---@field grey "#7b8496"|string
----@field blue "#5ea1ff"|string
----@field green "#5eff6c"|string
----@field cyan "#5ef1ff"|string
----@field red "#ff6e5e"|string
----@field yellow "#f1ff5e"|string
----@field magenta "#ff5ef1"|string
----@field pink "#ff5ea0"|string
----@field orange "#ffbd5e"|string
----@field purple "#bd5eff"|string
+---@field blue "#0057d1"|string
+---@field green "#008b0c"|string
+---@field cyan "#008c99"|string
+---@field red "#d11500"|string
+---@field yellow "#997b00"|string
+---@field magenta "#d100bf"|string
+---@field pink "#f40064"|string
+---@field orange "#d17c00"|string
+---@field purple "#a018ff"|string
 
 ---@class CyberdreamColorDefault
----@field bg "#16181a"|string
----@field bgAlt "#1e2124"|string
----@field bgHighlight "#3c4048"|string
+---@field bg "#0b0c0f"|string
+---@field bgAlt "#0E0F13"|string
+---@field bgHighlight "#2E303E"|string
 ---@field bg_solid? string
----@field fg "#ffffff"|string
----@field grey "#7b8496"|string
----@field blue "#5ea1ff"|string
----@field green "#5eff6c"|string
----@field cyan "#5ef1ff"|string
----@field red "#ff6e5e"|string
----@field yellow "#f1ff5e"|string
----@field magenta "#ff5ef1"|string
----@field pink "#ff5ea0"|string
----@field orange "#ffbd5e"|string
----@field purple "#bd5eff"|string
+---@field fg "#D5D8DA"|string
+---@field grey "#6C6F93"|string
+---@field blue "#26BBD9"|string
+---@field green "#29D398"|string
+---@field cyan "#59E1E3"|string
+---@field red "#E95678"|string
+---@field yellow "#FAB795"|string
+---@field magenta "#EE64AC"|string
+---@field pink "#E9436D"|string
+---@field orange "#F09483"|string
+---@field purple "#B877DB"|string
 
 ---@class CyberdreamColors
 ---@field default CyberdreamColorDefault
 M.default = {
-    bg = "#16181a",
-    bgAlt = "#1e2124",
-    bgHighlight = "#3c4048",
-    fg = "#ffffff",
-    grey = "#7b8496",
-    blue = "#5ea1ff",
-    green = "#5eff6c",
-    cyan = "#5ef1ff",
-    red = "#ff6e5e",
-    yellow = "#f1ff5e",
-    magenta = "#ff5ef1",
-    pink = "#ff5ea0",
-    orange = "#ffbd5e",
-    purple = "#bd5eff",
+    bg = "#0b0c0f",                -- Editor background
+    bgAlt = "#0E0F13",             -- SideBar background
+    bgHighlight = "#2E303E",       -- Selection background
+    fg = "#D5D8DA",                -- Foreground (text)
+    grey = "#6C6F93",              -- Grey (indent guides, etc.)
+    blue = "#26BBD9",              -- Blue (function names, etc.)
+    green = "#29D398",             -- Green (ansi green)
+    cyan = "#59E1E3",              -- Cyan (ansi cyan)
+    red = "#E95678",               -- Red (error foreground)
+    yellow = "#FAB795",            -- Yellow (ansi yellow, strings)
+    magenta = "#EE64AC",           -- Magenta (ansi magenta)
+    pink = "#E9436D",              -- Pink (active links)
+    orange = "#F09483",            -- Orange (constants)
+    purple = "#B877DB",            -- Purple (keywords)
 }
 
 ---@class CyberdreamColors
 ---@field light CyberdreamColorLight
 M.light = {
-    bg = "#ffffff",
-    bgAlt = "#eaeaea",
-    bgHighlight = "#acacac",
-    fg = "#16181a",
-    grey = "#7b8496",
-    blue = "#0057d1",
-    green = "#008b0c",
-    cyan = "#008c99",
-    red = "#d11500",
-    yellow = "#997b00",
-    magenta = "#d100bf",
-    pink = "#f40064",
-    orange = "#d17c00",
-    purple = "#a018ff",
+    bg = "#ffffff",                -- White background (light mode)
+    bgAlt = "#eaeaea",             -- Light alternative background
+    bgHighlight = "#acacac",       -- Light mode highlight background
+    fg = "#16181a",                -- Dark text for light mode
+    grey = "#7b8496",              -- Neutral grey
+    blue = "#0057d1",              -- Blue (light mode)
+    green = "#008b0c",             -- Green (light mode)
+    cyan = "#008c99",              -- Cyan (light mode)
+    red = "#d11500",               -- Red (light mode)
+    yellow = "#997b00",            -- Yellow (light mode)
+    magenta = "#d100bf",           -- Magenta (light mode)
+    pink = "#f40064",              -- Pink (light mode)
+    orange = "#d17c00",            -- Orange (light mode)
+    purple = "#a018ff",            -- Purple (light mode)
 }
 
 return M
